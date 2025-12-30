f32b3f154e4208597836c05dd7060ebc940d5aa6
ws3917 <ws3917@outlook.com>
2025-12-30 11:55:59 +0800

    for safety

--- 2-下载文本.bat
+++ 2-下载文本.bat
@@ -1,6 +1,8 @@
 @echo off
-set TOKEN=wlu_pI3gqtECKsngOn8V9ju78Sba4erdJnKPbqPg
-set URL=https://122.152.232.88/api/translations/deltarune
+rem File: secrets.bat
+rem set TOKEN=wlu_xxx
+rem set URL=https://xxxx/
+call secrets.bat
 start curl -H "Authorization: Token %TOKEN%" %URL%/ch1/en/file/ -o ./workspace/ch1/imports/text_src/en.json
 start curl -H "Authorization: Token %TOKEN%" %URL%/ch1/zh_Hans/file/ -o ./workspace/ch1/imports/text_src/cn.json
 start curl -H "Authorization: Token %TOKEN%" %URL%/ch2/en/file/ -o ./workspace/ch2/imports/text_src/en.json

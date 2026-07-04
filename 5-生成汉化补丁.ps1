c4009f73370f003cc1eeee80e06f8fa699a7eca5
ws3917 <ws3917@outlook.com>
2026-07-04 13:51:55 +0800

    [RC 2] 最终文本微调

--- 5-生成汉化补丁.ps1
+++ 5-生成汉化补丁.ps1
@@ -11,7 +11,7 @@ Set-StrictMode -Version Latest
 
 # ---------- 时间 ----------
 $fixedTime = Get-Date -Format "yyyy-MM-dd HH:mm"
-$date      = "RC-2"
+$date      = (Get-Date -Format "yyMMdd") + "-ch5"
 $ts        = Get-Date $fixedTime
 
 Write-Host "Build time : $fixedTime"

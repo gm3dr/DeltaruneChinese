4a9d6efa681609951dad6edfa0d4bfe7ce3c1c0d
SystemFileB <wangz7890@163.com>
2026-08-13 12:15:54 +0800

    修复 2-下载文本.ps1 无法在 Windows PowerShell 使用的问题，为其它文件添加 BOM

--- 2-下载文本.ps1
+++ 2-下载文本.ps1
@@ -1,15 +1,15 @@
-# 2-下载文本.ps1
-
-Remove-Item Alias:curl -Force -ErrorAction SilentlyContinue # 防止在旧版本 PowerShell 调用到 Invoke-WebRequest
+﻿# 2-下载文本.ps1
 
 # 内部成员可以创建 secrets.ps1，输入TOKEN拉取最新文本
 # $env:TOKEN = "..."
 # $env:URL = "..."
 if (Test-Path "secrets.ps1") { . ./secrets.ps1 }
 
+$curl = (Get-Command -CommandType Application -Name curl -ErrorAction SilentlyContinue)[0] # 防止在旧版本 PowerShell 调用到 Invoke-WebRequest
+
 $chapters = @("ch1", "ch2", "ch3", "ch4", "ch5")
 foreach ($c in $chapters) {
     Write-Host "Downloading $c..."
-    curl -H "Authorization: Token $env:TOKEN" "$($env:URL)/api/translations/deltarune/$c/en/file/" -o "./workspace/$c/imports/text_src/en.json"
-    curl -H "Authorization: Token $env:TOKEN" "$($env:URL)/api/translations/deltarune/$c/zh_Hans/file/" -o "./workspace/$c/imports/text_src/cn.json"
+    & $curl -H "Authorization: Token $env:TOKEN" "$($env:URL)/api/translations/deltarune/$c/en/file/" -o "./workspace/$c/imports/text_src/en.json"
+    & $curl -H "Authorization: Token $env:TOKEN" "$($env:URL)/api/translations/deltarune/$c/zh_Hans/file/" -o "./workspace/$c/imports/text_src/cn.json"
 }
